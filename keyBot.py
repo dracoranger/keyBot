ce883a942b9a9e3262b715dc9d2de97f9ca0d20a
Draco Ranger <dracoranger@hotmail.com>
2017-11-28 22:29:39 -0500

    V1.0 actual
    Uploaded a bad version before.  Sorry

--- keyBot.py
+++ keyBot.py
@@ -7,79 +7,105 @@ import asyncio
 import time
 #import logging
 
-client=discord.Client()
-bot=commands.Bot(command_prefix="!",description="")
+client = discord.Client()
+bot = commands.Bot(command_prefix="!",description="")
 
 BOT_FOLDER=""
 
-config=open('botData.txt','r')
-temp=config.readlines() #push to array or do directly
-token=temp[0][:-1]
+config = open('botData.txt','r')
+conf = config.readlines() #push to array or do directly
+token = conf[0][:-1]
 print(token)
-keysName=temp[1][:-1]
+keysName = conf[1][:-1]
 print(keysName)
-usedKeys=temp[2][:-1]
+usedKeys = conf[2][:-1]
 print(usedKeys)
-temp=open(keysName,'w+')
-temp.close
-temp=open(usedKeys,'w+')
-temp.close
+channelNum = conf[3][:-1]
+userComp = conf[4][:-1]
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
+    print('Logged in as ' + client.user.name)
     print(client.user.id)
     print('------')
+    message = client.user.name + " is up and running!"
+    await client.send_message(client.get_channel(channelNum), message)
 
 @client.event
 async def on_message(message):
+    global channelNum
     if message.author == client.user:
         return
-    if message.content.startswith('!keylist'):
-        keys = open(keysName,'r+')
-        keylist=keys.readlines()
-        keys.close
-        temp=''
-        for i in keylist:
-            temp=temp+i.split(',')[0]#should only show name
-        await client.send_message(message.channel, temp)
-    if message.content.startswith('!take'):
-        item = message.content[5:]
-        keys = open(keysName,'r+')
-        keylist=keys.readlines()
-        keys.close
-        temp=''
-        gib=''
-        newKeys=''
-        for i in keylist:
-            if i.split(',')[0]==item:
-                if gib == '':
-                    gib=i
+    if message.channel == client.get_channel(channelNum) or message.channel.is_private:
+        '''
+        prints the list of keys
+        '''
+        if message.content.startswith('!keylist'):
+            keys = open(keysName,'r')
+            keylist = keys.readlines()
+            keys.close()
+            temp = ''
+            for i in keylist:
+                temp = temp+i.split(',')[0]+'\n'#should only show name
+            if temp == '':
+                temp = 'No keys in storage'
+            await client.send_message(message.channel, temp)
+        '''
+        prints all commands
+        '''
+        if message.content.startswith('!help'):
+            keylist = "!keylist = prints a list of games that have keys, works in either server or in pms"
+            gib = "!gib [gameName] [key]= gives a key to the bot, only works in pms"
+            take = "!take [gameName] = messages you with the game's key, works only in server, recieve key in pm, message posted to server"
+            ret = keylist+'\n'+gib+'\n'+take+'\n'
+            await client.send_message(message.channel, ret)
+    if message.channel == client.get_channel(channelNum):
+        '''
+        gives user a key
+        '''
+        if message.content.startswith('!take'):
+            item = message.content[6:]
+            keys = open(keysName,'r+')
+            keylist = keys.readlines()
+            keys.close()
+            temp = ''
+            gib = ''
+            for i in keylist:
+                if i.split(',')[0]==item:
+                    if gib == '':
+                        gib = i
+                    else:
+                        temp = temp+i
                 else:
-                    temp=temp+i
-        await client.send_message(message.author,gib)
-        addToUsed= open(usedKeys,'a')
-        write(gib)
-        addToUsed.close
-        a=open(keysName,'w')
-        write(temp)
-        a.close
-    if message.content.startswith('!gib'):
-        item = message.content[4:]
-        temp=item.split(' ')
-        name=''
-        for i in range(0,len(temp)-1):
-            name=name+temp[i]
-        key = temp[len(temp)-1]
-        await client.send_message(message.author,"Thank you!\n I recieved "+name+" with a key of "+ key)
-    if message.content.startswith('!help'):
-        keylist="!keylist = prints a list of games that have keys"
-        gib="!gib [gameName] [key]= gives a key to the bot"
-        take="!take [gameName] = messages you with the game's key"
-        ret=keylist+'\n'+gib+'\n'+take+'\n'
-        await client.send_message(message.channel, ret)
-
+                    temp = temp+i
+            if gib == '':
+                publicMessage = "Item requested is not avalible"
+                gib = "Not avalible.  Please tell Draco if this is wrong"
+            else:
+                publicMessage = "Someone has claimed " + gib.split(',')[0]
+            await client.send_message(message.author,gib)
+            await client.send_message(client.get_channel(channelNum), publicMessage)
+            addToUsed = open(usedKeys, 'a')
+            addToUsed.write(gib+'\n')
+            addToUsed.close()
+            a = open(keysName, 'w')
+            a.write(temp)
+            a.close()
+    if message.channel.is_private:
+        '''
+        takes a key from a user
+        '''
+        if message.content.startswith('!gib'):
+            item = message.content[4:]
+            temp = item.split(' ')
+            name = ''
+            for i in range(0,len(temp)-1):
+                name = name+temp[i]
+            key = temp[len(temp)-1]
+            await client.send_message(message.author,"Thank you!\n I recieved "+name+" with a key of "+ key)
+            a = open(keysName, 'a')
+            a.write(name+','+key+'\n')
+            a.close()
     '''
         {
     "id": "162701077035089920",
